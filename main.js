8e821634e6ca5c21f8adf291c26ea1433575b955
Andrew McPherson <ehgoodenough@gmail.com>
2019-10-08 12:05:00 -0700

    Migrated http requests from callbacks to promises. (#1)

--- main.js
+++ main.js
@@ -1,22 +1,14 @@
-var microajax = require("microajax")
-var strip_ansi = require("strip-ansi")
+const fetchquest = require("fetchquest")
+const strip_ansi = require("strip-ansi")
 
 module.exports = function(callback) {
-    microajax("stats.json", function(response) {
-        if(!!response && response.status == 200) {
-            var stats = JSON.parse(response.responseText)
+    fetchquest({"url": "stats.json"}).then((stats) => {
+        stats.errors.forEach(function(error) {
+            (console.error || console.log)(strip_ansi(error))
+        })
 
-            stats.errors.forEach(function(error) {
-                (console.error || console.log)(strip_ansi(error))
-            })
-
-            stats.warnings.forEach(function(warning) {
-                (console.warn || console.log)(strip_ansi(warning))
-            })
-
-            if(callback != undefined) {
-                callback(stats)
-            }
-        }
+        stats.warnings.forEach(function(warning) {
+            (console.warn || console.log)(strip_ansi(warning))
+        })
     })
 }
